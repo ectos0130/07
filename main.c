58b725974aa88d91ab64cdb55845de693a224dbe
ectos0130 <ectos0130@naver.com>
2023-10-19 15:35:04 +0900

    04

--- main.c
+++ main.c
@@ -2,13 +2,18 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int y;
+void f(void);
+int i;
 int main(int argc, char *argv[]){
-	int flag = 1;
-	while (flag != 0){
-		y = 3;
-		flag = 0;
+	for(i=0; i<5; i++)
+	{
+		f();
 	}
-	y = 4;
 	return 0;
+}
+
+void f(void)
+{
+	for (i=0; i<10; i++)
+		printf("#");
 }
